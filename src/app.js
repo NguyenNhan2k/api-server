6d9b73245d51bdc6e66914a848e55e855e5b38eb
nhan <nhan2k@gmail.com>
2023-01-11 16:22:23 +0700

    import library in file app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const route = require('./api/routes')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const app = express()
 dotenv.config()
 const port = process.env.PORT || 3000
+
+app.use(cookieParser())
+app.use(cors())
+app.use(express.json())
 // use routes
 route(app)
 app.listen(port, ()=> {
