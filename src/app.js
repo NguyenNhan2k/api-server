fe7a843e230c145c1d62c46c63d887c71dc7b0d1
nhan <nhan2k@gmail.com>
2023-02-02 23:02:21 +0700

    config and connect database use libary sequelize

--- src/app.js
+++ src/app.js
@@ -3,15 +3,20 @@ const dotenv = require('dotenv')
 const route = require('./api/routes')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+
+
 const app = express()
 dotenv.config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(cookieParser())
 app.use(cors())
 app.use(express.json())
 // use routes
 route(app)
+
+// Connect database
+
 app.listen(port, ()=> {
     console.log(`sever listening on port ${port}`)
 })
